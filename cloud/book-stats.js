97fd97014baa5d3afec3bfaf0544fe1b0785fe1c
Bin Lin <cold.lin@gmail.com>
2017-10-25 20:23:37 +0100

    record recommendTimes

--- cloud/book-stats.js
+++ cloud/book-stats.js
@@ -3,8 +3,11 @@ Parse.Cloud.define("incrementFeaturedBookStats", function(request, response) {
 	var bookQuery =new Parse.Query("PublishedBook");
 
 	var bookId =request.params.bookRemoteId;
-	var bookLikeAddCount = request.params.likedTimes || 1;
-	var bookReadAddCount = request.params.readTimes || 1;
+	var bookLikeAddCount = request.params.likedTimes || 0;
+	var bookReadAddCount = request.params.readTimes || 0;
+	var bookRecommendAddCount = request.params.recommendTimes || 0;
+	var username = request.params.username;
+
 	console.log("search with ids:"+bookId);
 	bookQuery.equalTo("objectId",bookId);
 	bookQuery.limit(1);
@@ -14,7 +17,9 @@ Parse.Cloud.define("incrementFeaturedBookStats", function(request, response) {
     		  	var book = results[0];
     			book.increment("playedTimes", bookReadAddCount);
     			book.increment("likedTimes", bookLikeAddCount);
+    			book.increment("recommendTimes", bookRecommendAddCount);
     			book.save(null, { useMasterKey: true });
+    			recordUserEvent(username, book, bookReadAddCount>0, bookLikeAddCount>0, bookRecommendAddCount>0);
     			response.success("incrementBookStats with Book only");
     		},
     		error: function() {
@@ -57,7 +62,7 @@ Parse.Cloud.define("incrementFeaturedBookPlay", function(request, response) {
     		  	var book = results[0];
     			book.increment("playedTimes");
     			book.save(null, { useMasterKey: true });
-    			recordUserBookPlay(username, book, true, false);
+    			recordUserEvent(username, book, true, false, false);
     			response.success("incrementBookPlay with Book only");
     		},
     		error: function() {
@@ -79,7 +84,7 @@ Parse.Cloud.define("incrementFeaturedBookLike", function(request, response) {
     		  	var book = results[0];
     			book.increment("likedTimes");
     			book.save(null, { useMasterKey: true });
-    			recordUserBookPlay(username, book, false, true);
+    			recordUserEvent(username, book, false, true, false);
     			response.success("incrementBookLikes with Book only");
     		},
     		error: function() {
@@ -88,7 +93,7 @@ Parse.Cloud.define("incrementFeaturedBookLike", function(request, response) {
 	});
 });
 
-function recordUserBookPlay(username, book, isRead, isLike){
+function recordUserEvent(username, book, isRead, isLike, isRecommend){
 				if(username && book){
 								var UserEventClass = Parse.Object.extend("UserEvent");
         userEvent = new UserEventClass();
@@ -106,6 +111,9 @@ function recordUserBookPlay(username, book, isRead, isLike){
         if(isLike){
          userEvent.set("like", true);
         }
+        if(isRecommend){
+          userEvent.set("recommend", true);
+         }
         userEvent.save(null, { useMasterKey: true });
         console.log("recorded user event:" + userProfile);
 				}
